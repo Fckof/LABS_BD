9252c8e5139a24c49af94f88043487d3c65c0165
Fckof <mr.pchelintsev@bk.ru>
2021-03-26 17:58:58 +0300

    нр

---
+++ GameDev/developercomand.php
@@ -0,0 +1,24 @@
+<?php 
+	$userRoot = "root";
+	$passRoot = "root";
+	$servername="localhost";
+	$database="gamedev";
+
+$conn = new mysqli($servername, $userRoot, $passRoot, $database) or die("Не подрубилось...");
+
+$result=$conn->query("SELECT * FROM `команда разработчиков`");
+
+if (count($result)==0){
+		echo "Нет записей";
+	}
+
+print "<table border='1' style='color:white;font-size:25px;margin:30px auto;border:3px solid orange;'><th>ID разработчика</th><th>Название</th><th>Дата начала работы</th>";
+
+while ($row=$result->fetch_assoc()) {
+		print "<tr><td>".$row["ID разработчика"]."</td><td>".$row["Название"]."</td><td>".$row["Дата начала работы"]."</td></tr>";
+	}
+
+print "</table>";
+
+$conn->close();
+ ?>
