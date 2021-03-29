422ce685bb211c976e2d94d8b6973b6cba1829d6
Fckof <mr.pchelintsev@bk.ru>
2021-03-29 18:25:16 +0300

    g

--- GameDev/table/games.php
+++ GameDev/table/games.php
@@ -12,10 +12,10 @@ if (count($result)==0){
 		echo "Нет записей";
 	}
 
-print "<table border='1' style='color:white;font-size:25px;margin:30px auto;border:3px solid orange;'><th>ID игры</th><th>ID разработчика</th><th>Название</th><th>Дата начала работы</th><th>Дата окончания работы</th><th>Дата выпуска</th>";
+print "<table border='1' style='color:white;font-size:25px;margin:30px auto;border:3px solid orange;'><th>ID игры</th><th>ID разработчика</th><th>Название</th><th>Рейтинг</th><th>Дата начала работы</th><th>Дата окончания работы</th><th>Дата выпуска</th>";
 
 while ($row=$result->fetch_assoc()) {
-		print "<tr><td>".$row["ID игры"]."</td><td>".$row["ID разработчика"]."</td><td>".$row["Название"]."</td><td>".$row["Дата начала работы"]."</td><td>".$row["Дата окончания работы"]."</td><td>".$row["Дата выпуска"]."</td></tr>";
+		print "<tr><td>".$row["ID игры"]."</td><td>".$row["ID разработчика"]."</td><td>".$row["Название"]."</td><td>".$row["Рейтинг"]."</td><td>".$row["Дата начала работы"]."</td><td>".$row["Дата окончания работы"]."</td><td>".$row["Дата выпуска"]."</td></tr>";
 	}
 
 print "</table>";
