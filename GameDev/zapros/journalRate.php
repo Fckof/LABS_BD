c3f644caabb21e92cbac257076f9282e9ecec316
Fckof <mr.pchelintsev@bk.ru>
2021-05-31 01:56:33 +0300

    i

--- GameDev/zapros/journalRate.php
+++ GameDev/zapros/journalRate.php
@@ -8,10 +8,10 @@ if (count($result)==0){
 	}
 	print "<h1 style='text-align:center;color:white;'>Изменение рейтинга</h1>";
 
-print "<table border='1' style='color:white;font-size:25px;margin:30px auto;border:3px solid orange;'><th>ID игры</th><th>Старый рейтинг</th><th>Новый рейтинг</th><th>Дата до обновления</th><th>Дата обновления</th>";
+print "<table border='1' style='color:white;font-size:25px;margin:30px auto;border:3px solid orange;'><th>Название игры</th><th>Старый рейтинг</th><th>Новый рейтинг</th><th>Дата до обновления</th><th>Дата обновления</th>";
 
 while ($row=$result->fetch_assoc()) {
-		print "<tr><td>".$row["ID игры"]."</td><td>".$row["Старый рейтинг"]."</td><td>".$row["Новый рейтинг"]."</td><td>".$row["Дата до обновления"]."</td><td>".$row["Дата обновления"]."</td></tr>";
+		print "<tr><td>".$row["Название"]."</td><td>".$row["Старый рейтинг"]."</td><td>".$row["Новый рейтинг"]."</td><td>".$row["Дата до обновления"]."</td><td>".$row["Дата обновления"]."</td></tr>";
 	}
 
 print "</table>";
